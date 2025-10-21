ab959f27eb9fb330d785c7464b453f982b031b05
ArchieThe <a1904555@adelaide.edu.au>
2025-10-21 19:30:44 +1030

    la

--- game.cpp
+++ game.cpp
@@ -3,3 +3,15 @@
 #include <limits>
 #include <thread>
 #include <chrono>
+
+
+// Constructor to set up the player
+Game::Game() {
+    std::string player_name;
+    std::cout << "Enter your player's name: ";
+    std::getline(std::cin, player_name);
+    player_.setName(player_name);
+    std::cout << "Welcome, " << player_.getName() << "!\n";
+}
+
+
